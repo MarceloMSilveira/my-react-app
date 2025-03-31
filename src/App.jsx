63d7bc5603a8af78d5f4e17e5633aaf2b49a9afa
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-03-31 15:17:12 -0300

    ex 280

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
-  return <h1>React Test</h1>;
+  return (
+    <>
+      <h1>React Test</h1>
+      <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+      </ul>
+    </>
+);
 }
 
 export default App;
