a14724f3ffc7db8bc7f49e4d1ea57c9e6abcfcfd
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-03-31 14:11:02 -0300

    ex 280

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-   <StrictMode>
-     <App />
-   </StrictMode>,
+   <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Apple</li>
+    </ul>
+   </div>
 )
