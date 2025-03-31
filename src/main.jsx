6428cb74e4fb39461091c973b01b662387bd8fb9
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-03-31 15:20:11 -0300

    set main

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Apple</li>
-    </ul>
-   </div>
+   <StrictMode>
+    <App></App>
+   </StrictMode>
 )
