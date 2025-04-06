c8cedba4f1c265c7e836c2fa6aab8fbc948c4cc6
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-04-06 20:20:21 -0300

    set ternary operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,28 +2,12 @@ import Login from "./Login";
 
 let isLoged = false;
 
-function checkLogin() {
-  if (isLoged) {
-    return <h1>Hello</h1>  
-   } else {
-     return <Login /> 
-   }
-}
-
 function App() {
   return (
     <div className="container">
-      {checkLogin()}
+      {isLoged ? <h1>Hello </h1> : <Login />}
     </div>
   )
 }
 
 export default App;
-
-//1. Apply CSS styles to App.jsx component
-//to match the appearance on the completed app:
-//https://c6fkx.csb.app/
-//2. Extract the contact card as a reusable Card component.
-//3. Use props to render the default Beyonce contact card
-//so the Card component can be reused for other contacts.
-//4. Import the contacts.js file to create card components.
