feb1dc42fda67765039308a2df97375ff6dee960
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-04-06 07:30:10 -0300

    set cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import Card from './Card'
+import contacts from '../contact';
 
 function App() {
   return (
     <>
-      <h1>My contacts</h1>
+      <h1 className="heading">My contacts</h1>
       <Card 
         name = "Beyonce"
         img = {{
@@ -40,3 +41,11 @@ function App() {
 }
 
 export default App;
+
+//1. Apply CSS styles to App.jsx component
+//to match the appearance on the completed app:
+//https://c6fkx.csb.app/
+//2. Extract the contact card as a reusable Card component.
+//3. Use props to render the default Beyonce contact card
+//so the Card component can be reused for other contacts.
+//4. Import the contacts.js file to create card components.
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,15 @@
 function Card ({name, img, phone, email}) {
   return (
-    <div>
-      <h2>{name}</h2>
-      <img src={img.src} alt={img.alt} />
-      <p>{phone}</p>
-      <p>{email}</p>
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{name}</h2>
+        <img className="circle-img" src={img.src} alt={img.alt} />
+      </div>
+      <div className="bottom">
+        <p className="info">{phone}</p>
+        <p className="info">{email}</p>
+      </div>
+      
     </div>
   )
 }
