9439fbbd95c05c831e410bca4e8eb8492a9d1ed6
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-04-07 09:56:59 -0300

    set handle click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 function App() {
 
   const [isOver,setIsOver] = useState(false);
+  const [userName, setName] = useState("");
 
   function handleOver() {
     setIsOver(true);
@@ -12,13 +13,29 @@ function App() {
     setIsOver(false);
   }
 
+  function handleClick() {
+    const value = document.getElementById("name").value
+    setName(value);
+  }
+
+  function handleInput(evt) {
+    console.log(evt.target.value)
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>Hello {userName}</h1>
+      <input 
+        id="name"
+        type="text" 
+        placeholder="What's your name?" 
+        onChange={handleInput}
+        value={userName}
+      />
       <button  
         onMouseOver={handleOver}
         onMouseOut={handleOut}
+        onClick={handleClick}
         style={{backgroundColor: isOver && "black"}}
       >Submit</button>
     </div>
