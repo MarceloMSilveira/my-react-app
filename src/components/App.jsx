bfeb73ee8b60633fcb6877a1833a98929f004975
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-04-06 21:37:53 -0300

    307 useState practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,36 @@ import { useState } from 'react';
 
 function App() {
 
-  const [count, setCount] = useState(0)
-
-  function handlePlus() {
-    setCount(count + 1)
-  }
-
-  function handleMinus() {
-    setCount(count - 1)
-  }
-
   return (
     <div className="container">
-      <h1>{count}</h1>
-      <button onClick={handlePlus}>+</button>
-      <button onClick={handleMinus}>-</button>
+      <h1>TIME</h1>
+      <button>Get Time</button>
     </div>
   )
 }
 
 export default App;
+
+//Challenge:
+//1. Given that you can get the current time using:
+let time = new Date().toLocaleTimeString();
+console.log(time);
+//Show the latest time in the <h1> when the Get Time button
+//is pressed.
+
+//2. Given that you can get code to be called every second
+//using the setInterval method.
+//Can you get the time in your <h1> to update every second?
+
+//e.g. uncomment the code below to see Hey printed every second.
+// function sayHi() {
+//   console.log("Hey");
+// }
+// setInterval(sayHi, 1000);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
 
